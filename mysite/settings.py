7d12a523eb559eebb5cd404370fb9802b0ec45d3
gardenclass <ggarden.lee@gmail.com>
2020-10-20 21:05:02 +0900

    bootstrap update

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'a*rs9lowx0oswl%bayynf5xzzxrs)tqvrvq$6(o-nxxby5pt90'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
